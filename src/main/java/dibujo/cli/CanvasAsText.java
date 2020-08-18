ab784d70bceb8f9e68d790f71b4d53f8188e23ae
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-08-18 14:48:59 -0300

    Extract delegate

---
+++ src/main/java/dibujo/cli/CanvasAsText.java
@@ -0,0 +1,54 @@
+package dibujo.cli;
+
+import dibujo.core.Canvas;
+import dibujo.core.Position;
+
+import java.io.PrintStream;
+
+public class CanvasAsText {
+
+    void draw(PrintStream out, Canvas canvas) {
+        out.println();
+
+        for (int i = 0; i <= canvas.getWidth(); i++) {
+            out.print("-");
+        }
+        out.print("-");
+
+        out.println();
+
+        int x = 1, y = 1;
+        while (x >= 1 && x <= canvas.getWidth() && y >= 1 && y <= canvas.getHeight()) {
+            Position position = canvas.getPosition(x, y);
+            if (position.getX() == 1) {
+                if (position.getY() > 1) {
+                    out.println("|");
+                }
+                out.print("|");
+            }
+
+            if (position.getColor() != null) {
+                out.print(position.getColor());
+            } else if (position.isFilled()) {
+                out.print("x");
+            } else {
+                out.print(" ");
+            }
+            x++;
+            if (x >= canvas.getWidth() + 1) {
+                x = 1;
+                y++;
+            }
+        }
+
+        out.println("|");
+        out.print("-");
+
+        for (int i = 0; i < canvas.getWidth(); i++) {
+            out.print("-");
+        }
+        out.print("-");
+
+        out.println();
+    }
+}
--- src/main/java/dibujo/cli/Main.java
+++ src/main/java/dibujo/cli/Main.java
@@ -12,6 +12,7 @@ import java.util.regex.Pattern;
 public class Main {
 
     private final Quit quit = new Quit();
+    private final CanvasAsText canvasAsText = new CanvasAsText();
     private Canvas canvas;
 
     public void run(InputStream in, PrintStream out, PrintStream err) {
@@ -35,7 +36,7 @@ public class Main {
                         err.println("Invalid command: " + line + "\n");
                     }
 
-                    draw(out, this.canvas);
+                    canvasAsText.draw(out, canvas);
 
                     out.print("\nenter command: ");
 
@@ -45,51 +46,6 @@ public class Main {
                 }
             }
         }
-    }
-
-    private void draw(PrintStream out, Canvas canvas) {
-        out.println();
-
-        for (int i = 0; i <= canvas.getWidth(); i++) {
-            out.print("-");
-        }
-        out.print("-");
-
-        out.println();
-
-        int x = 1, y = 1;
-        while (x >= 1 && x <= canvas.getWidth() && y >= 1 && y <= canvas.getHeight()) {
-            Position position = canvas.getPosition(x, y);
-            if (position.getX() == 1) {
-                if (position.getY() > 1) {
-                    out.println("|");
-                }
-                out.print("|");
-            }
-
-            if (position.getColor() != null) {
-                out.print(position.getColor());
-            } else if (position.isFilled()) {
-                out.print("x");
-            } else {
-                out.print(" ");
-            }
-            x++;
-            if (x >= canvas.getWidth() + 1) {
-                x = 1;
-                y++;
-            }
-        }
-
-        out.println("|");
-        out.print("-");
-
-        for (int i = 0; i < canvas.getWidth(); i++) {
-            out.print("-");
-        }
-        out.print("-");
-
-        out.println();
     }
 
     private void bucketFill(String line) {
