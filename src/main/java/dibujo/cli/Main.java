142c49132d774d1fee3d7803fe483181e9207977
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-08-18 14:41:05 -0300

    Move method

--- src/main/java/dibujo/cli/Main.java
+++ src/main/java/dibujo/cli/Main.java
@@ -35,7 +35,7 @@ public class Main {
                         err.println("Invalid command: " + line + "\n");
                     }
 
-                    draw(out);
+                    canvas.draw(out, this);
 
                     out.print("\nenter command: ");
 
@@ -45,51 +45,6 @@ public class Main {
                 }
             }
         }
-    }
-
-    private void draw(PrintStream out) {
-        out.println();
-
-        for (int i = 0; i <= canvas.getWidth(); i++) {
-            out.print("-");
-        }
-        out.print("-");
-
-        out.println();
-
-        int x = 1, y = 1;
-        while (x >= 1 && x <= canvas.getWidth() && y >= 1 && y <= canvas.getHeight()) {
-            Position position = canvas.getPosition(x, y);
-            if (position.getX() == 1) {
-                if (position.getY() > 1) {
-                    out.println("|");
-                }
-                out.print("|");
-            }
-
-            if (position.getColor() != null) {
-                out.print(position.getColor());
-            } else if (position.isFilled()) {
-                out.print("x");
-            } else {
-                out.print(" ");
-            }
-            x++;
-            if (x >= canvas.getWidth() + 1) {
-                x = 1;
-                y++;
-            }
-        }
-
-        out.println("|");
-        out.print("-");
-
-        for (int i = 0; i < canvas.getWidth(); i++) {
-            out.print("-");
-        }
-        out.print("-");
-
-        out.println();
     }
 
     private void bucketFill(String line) {
--- src/main/java/dibujo/core/Canvas.java
+++ src/main/java/dibujo/core/Canvas.java
@@ -1,5 +1,8 @@
 package dibujo.core;
 
+import dibujo.cli.Main;
+
+import java.io.PrintStream;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -117,4 +120,48 @@ public class Canvas {
         fill(positionToFill.left(), fillColor, visited);
     }
 
+    public void draw(PrintStream out, Main main) {
+        out.println();
+
+        for (int i = 0; i <= getWidth(); i++) {
+            out.print("-");
+        }
+        out.print("-");
+
+        out.println();
+
+        int x = 1, y = 1;
+        while (x >= 1 && x <= getWidth() && y >= 1 && y <= getHeight()) {
+            Position position = getPosition(x, y);
+            if (position.getX() == 1) {
+                if (position.getY() > 1) {
+                    out.println("|");
+                }
+                out.print("|");
+            }
+
+            if (position.getColor() != null) {
+                out.print(position.getColor());
+            } else if (position.isFilled()) {
+                out.print("x");
+            } else {
+                out.print(" ");
+            }
+            x++;
+            if (x >= getWidth() + 1) {
+                x = 1;
+                y++;
+            }
+        }
+
+        out.println("|");
+        out.print("-");
+
+        for (int i = 0; i < getWidth(); i++) {
+            out.print("-");
+        }
+        out.print("-");
+
+        out.println();
+    }
 }
