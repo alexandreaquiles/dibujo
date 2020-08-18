159315669127ec688f17bb4500206722e352a985
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-08-18 14:43:59 -0300

    undoing Change signature and move method

--- src/main/java/dibujo/core/Canvas.java
+++ src/main/java/dibujo/core/Canvas.java
@@ -1,6 +1,5 @@
 package dibujo.core;
 
-import java.io.PrintStream;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -118,48 +117,4 @@ public class Canvas {
         fill(positionToFill.left(), fillColor, visited);
     }
 
-    public void draw(PrintStream out) {
-        out.println();
-
-        for (int i = 0; i <= getWidth(); i++) {
-            out.print("-");
-        }
-        out.print("-");
-
-        out.println();
-
-        int x = 1, y = 1;
-        while (x >= 1 && x <= getWidth() && y >= 1 && y <= getHeight()) {
-            Position position = getPosition(x, y);
-            if (position.getX() == 1) {
-                if (position.getY() > 1) {
-                    out.println("|");
-                }
-                out.print("|");
-            }
-
-            if (position.getColor() != null) {
-                out.print(position.getColor());
-            } else if (position.isFilled()) {
-                out.print("x");
-            } else {
-                out.print(" ");
-            }
-            x++;
-            if (x >= getWidth() + 1) {
-                x = 1;
-                y++;
-            }
-        }
-
-        out.println("|");
-        out.print("-");
-
-        for (int i = 0; i < getWidth(); i++) {
-            out.print("-");
-        }
-        out.print("-");
-
-        out.println();
-    }
 }
